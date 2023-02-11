49d3fd5f646d00647b72c3c90efeaaa3959539a7
edwinacos55 <edwinacos55@gmail.com>
2023-02-11 16:13:52 -0500

    commit tree 8f864db181a255f72e711f5a6c30d94da7778552

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ let form= document.querySelector("form")
+ form.addEventListener("submit", (e) => {
+  e.preventDefault() 
+  addTodo(e.target.new_task_description.value)
+
+  form.reset();
+
+ })
+
+
+ function addTodo (todo) {
+  let li= document.createElement('li')
+  let btn= document.createElement('button')
+  btn.addEventListener("click", handleDelete)
+  btn.textContent= 'X';
+  li.textContent= todo + " ";
+  li.appendChild(btn);
+ document.querySelector('#tasks').appendChild(li)
+
+ }
+
+function handleDelete(e) {
+e.target.parentNode.remove();
+}
+ 
 });
